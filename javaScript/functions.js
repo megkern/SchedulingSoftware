facaaa61f18f1b7e5e8722928ef65af648bdbe24
megkern <meganelizabethkern@gmail.com>
2023-10-05 17:43:34 -0400

    Isolated style and functions into their own folders called CSS and javaScript

---
+++ javaScript/functions.js
@@ -0,0 +1,15 @@
+function clickAlert(clickbox_el) {
+  // alert("Hello"); /*"alert" is a javascript function that pops up a window with text in it*/
+  //var el = document.getElementById("specialID"); // declare a variable called el, "getElementById" is a function that selects the element within the webpage (document) that has the special id
+  //el.style.backgroundColor = "red"; //change the style, specifically the background color, of our variable called el to red.
+
+  //var clickbox_el = document.getElementById("specialID"); //What's the element that was clicked?
+  var backColor = clickbox_el.style.backgroundColor; //What's the background color of that element?
+  var defaultColor = "black";
+
+  if (backColor != "red") {
+    defaultColor = "red";
+  }
+
+  clickbox_el.style.backgroundColor = defaultColor; //Change it!
+}
