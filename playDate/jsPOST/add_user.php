5ac8fd4f51a3c3bfa32982a9a9d4f1d9881bb403
kelseykern <kelseykern@users.noreply.github.com>
2023-10-05 17:30:35 -0500

    update

---
+++ playDate/jsPOST/add_user.php
@@ -0,0 +1,15 @@
+<?php
+
+// get jsPOST data
+$name = $_POST['name'];
+
+// Connect to the database 
+include '../../connect/connect.php';
+
+// add the user
+
+// Close the connection 
+include '../../connect/disconnect.php';
+
+
+?>
