d7f2bff58cf2b69ee07505e2c1cd1ec0f145f32c
megkern <meganelizabethkern@gmail.com>
2023-10-05 18:00:36 -0400

    formatting and clean up

--- index.php
+++ index.php
@@ -1,22 +1,19 @@
-<!DOCTYPE html> <!--Library shit: Add plugins, javascript/css links, footers, etc.-->
+<!DOCTYPE html> <!--Library shit: Add plugins, javascript/css links, footers,-->
 <html lang="en"> <!--set the language-->
-<!--tag .class tag-->
 
 <head> <!--"head" like libraries and stuff tag-->
   <title>Make-A-Meeting</title> <!--This is the title of the tab-->
-  <meta charset="utf-8">
-  <!--An extra setting section (meta) describing the character set, utf-8 means unicode characters with 8 bits-->
+  <meta charset="utf-8"> <!--An extra setting section (meta) describing the character set, utf-8 means unicode characters with 8 bits-->
   <meta name="viewport" content="width=device-width, initial-scale=1"> <!---->
-  <!--Watch out! You can't comment in a "style" tag in this traditional way, you need to do this instead...-->
+  
+  <link rel="stylesheet" href="CSS/style.css">
 
-<link rel="stylesheet" href="CSS/style.css">
-
-<script type="text/javascript" src="javaScript/functions.js"></script>
+  <script type="text/javascript" src="javaScript/functions.js"></script>
 
 </head> <!--end of the "head" tag-->
 
 <body> <!--"body" tag-->
-<?php include "content.php";?>
+  <?php include "content.php"; ?>
 </body>
 
 </html>
