c2e047a9c840b256d878fa5a14dd120c0a3a8d5c
kelseykern <kelseykern@users.noreply.github.com>
2023-10-05 17:35:04 -0500

    update

--- playDate/jsPOST/add_user.php
+++ playDate/jsPOST/add_user.php
@@ -7,6 +7,13 @@ $name = $_POST['name'];
 include '../../connect/connect.php';
 
 // add the user
+$stmt = $mysqli->prepare("INSERT INTO user (display_name) VALUES (?)");
+$stmt->bind_param("s", $name);
+
+// Execute the SQL statement 
+if ($stmt->execute()) {
+    echo "user added! ~your friendly php output";
+}
 
 // Close the connection 
 include '../../connect/disconnect.php';
