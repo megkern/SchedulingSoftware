9ca3726a571fe609e8512ee7dbf21b8a6dd29ecf
kelseykern <kelseykern@users.noreply.github.com>
2023-10-05 17:26:50 -0500

    Add ajax

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
   
   <link rel="stylesheet" href="CSS/style.css">
 
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
   <script type="text/javascript" src="javaScript/functions.js"></script>
 
 </head> <!--end of the "head" tag-->
