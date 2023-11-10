ed0594f9ddce8bdb19e10787c314f669fcb4f331
megkern <meganelizabethkern@gmail.com>
2023-11-10 14:15:27 -0600

    comment:

--- dashboard.php
+++ dashboard.php
@@ -8,4 +8,12 @@
 <a href="index.php?current_page=create_meeting_page">
     Create Meeting 2
 </a>
+
+
+
+
+Landing Page: Welcome to Megan's Scheduling Software (bottons for create a new meeting, check on existing meeting, sign in)
+Dashboard ("sign in", has navigstion bar with meetings you are managing, outcomes and stats)
+Create Meeting Page (new meetings, set up time frame, dates, input inital values)
+Meeting Page (where people input their availability and see the accumulated availaiilty of others)
 -->
