67a1dc8065544ab5d1ea8e5672e0680cc4f387d3
megkern <meganelizabethkern@gmail.com>
2023-11-10 15:04:46 -0600

    change website address

--- nav_bar.php
+++ nav_bar.php
@@ -9,10 +9,9 @@ navigation bar
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
+      <a class="navbar-brand" href="https://simsignup.com">Sim Sign Up</a>
     </div>
     <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
       <li><a href="index.php?current_page=landing_page">Landing Page</a></li>
       <li><a href="index.php?current_page=dashboard_page">Dashboard</a></li>
       <li><a href="index.php?current_page=create_meeting_page">Create Meeting</a></li>
